0fbe78fb3d9f230ce5650e19effb1be3ca150b0d
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-07-18 19:12:15 +0300

    updated post bloc

--- lib/bloc/post_bloc.dart
+++ lib/bloc/post_bloc.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:bloc_api/core/network_client.dart';
 import 'package:bloc_api/core/post_client.dart';
+import 'package:bloc_api/repositories/post_repository.dart';
 import 'package:meta/meta.dart';
 
 import '../models/post.dart';
@@ -15,9 +16,9 @@ class PostBloc extends Bloc<PostEvent, PostState> {
     on<PostEvent>((event, emit) {});
 
     on<GetPostEvent>((event,emit) async {
-      PostClient client = PostClient();
+      PostRepository _repository = PostRepository();
 
-      List<Post> posts = await client.getPosts();
+      List<Post> posts = await _repository.getAllPosts();
 
       emit(GetPostState(posts));
     });
